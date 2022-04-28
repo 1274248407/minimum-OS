9cf23d5aa95e9963d842d940a549582958960b92
1274248407 <1274248407@qq.com>
2022-04-28 18:49:49 +0800

    added

---
+++ test/complie.bat
@@ -0,0 +1,2 @@
+nasm boot.asm -o boot.bin
+dd if=D:\proramming\mustly_book\Linux\Operating_system\Design and implementation of a 64 bit operating system\test\boot.bin of=D:\proramming\mustly_book\Linux\Operating_system\Design and implementation of a 64 bit operating system\test\boot.img count=1 bs=512 conv=notrunc
---
+++ test/run.bat
@@ -0,0 +1,1 @@
+D:\proramming\Virtual_machines\Bochs-2.6.11\bochs.exe -q -f D:\proramming\mustly_book\Linux\Operating_system\Design and implementation of a 64 bit operating system\test\borchsrc.txt
